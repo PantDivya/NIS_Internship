a98a24d1ed7e6cef1efe222a07ff718e6541f0eb
PantDivya <divyapant108@gmail.com>
2024-07-01 10:06:00 +0545

    crud operation

---
+++ CRUD/CRUD/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using CRUD.Models;
+using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmployeeEntities db = new EmployeeEntities();
+
+        /*[AcceptVerbs(HttpVerbs.Get | HttpVerbs.Post)]*/
+        public ActionResult Index()
+        {
+            //EmployeeViewModel viewModel = new EmployeeViewModel();
+
+            List<tblEmployee> employee = db.tblEmployees.ToList();
+            /*viewModel.EmployeeList = employee;
+            if(id!= null)
+            {
+                tblEmployee employee1 = db.tblEmployees.Find(id);
+                viewModel.Employee = employee1;
+                EditEmployee(viewModel.Employee);
+            }*/
+
+
+            //return View(viewModel);
+
+
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(tblEmployee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                var data = db.tblEmployees.Add(employee);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult UpdateEmployee(int? id)
+        {
+
+                var employee = db.tblEmployees.Find(id);
+                if (employee != null)
+                {
+                    return View(employee);
+
+                }
+                return HttpNotFound();
+            
+        }
+        [HttpPost]
+        public ActionResult EditEmployee(int id, tblEmployee employee) 
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
+        
+        public ActionResult DeleteEmployee(int? id)
+        {
+            var employee = db.tblEmployees.Find(id);
+            if (employee != null)
+            {
+                return View(employee);
+
+            }
+            return HttpNotFound();
+        }
+        [HttpPost]
+        public ActionResult Delete(tblEmployee employee, int? id)
+        {
+            db.Entry(employee).State = EntityState.Deleted;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+       
+    }
+}
---
+++ CRUD/CRUD/Models/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRUD.Models
+{
+    public class EmployeeViewModel
+    {
+        public List<tblEmployee> EmployeeList { get; set; }
+        public tblEmployee Employee { get; set; }
+
+    }
+}
