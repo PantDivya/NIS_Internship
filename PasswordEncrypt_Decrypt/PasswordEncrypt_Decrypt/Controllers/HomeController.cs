38ae0eb57b581ebdf5f7d6a3f46ae28f6ec2f2a6
PantDivya <divyapant108@gmail.com>
2024-07-31 14:21:17 +0545

    Password Encryption and Decryption

---
+++ PasswordEncrypt_Decrypt/PasswordEncrypt_Decrypt/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using PasswordEncrypt_Decrypt.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PasswordEncrypt_Decrypt.Controllers
+{
+    public class HomeController : Controller
+    {
+        MainEntities db = new MainEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(User_tbl user) 
+        {
+            var encryptedPassword = Encrypt(user.Password);
+            user.Password = encryptedPassword;
+            db.User_tbl.Add(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UserList()
+        {
+            var users = db.User_tbl.ToList();
+            foreach (var user in users)
+            {
+                user.EncryptedPassword = user.Password;
+                user.Password = Decrypt(user.Password);
+                db.User_tbl.Add(user);
+            }
+            return View(users);
+        }
+
+        private static string Encrypt(string clearText)
+        {
+            string encryptionKey = "MYNAMEISDIYVA780991";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+        private static string Decrypt(string cipherText)
+        {
+            string encryptionKey = "MYNAMEISDIYVA780991";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(encryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+    }
+}
---
+++ PasswordEncrypt_Decrypt/PasswordEncrypt_Decrypt/Models/PartialClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PasswordEncrypt_Decrypt.Models
+{
+    public partial class User_tbl
+    {
+        public string EncryptedPassword {  get; set; }
+    }
+}
